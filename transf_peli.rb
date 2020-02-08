b24022f17a1e6fc4aec730cc307fcf53e7e1e5ca
zzwd7d <zzwd7d@users.noreply.github.com>
2020-02-08 15:02:55 -0300

    version 2.11

--- transf_peli.rb
+++ transf_peli.rb
@@ -1,52 +1,73 @@
 
 def imdb2Peli (ximdb)
   k = Pelicula.new
-  k.attributes =  { :titulo => ximdb.title[0],
-                    :tituloOriginal => ximdb.title[2],
-                    :anio => ximdb.title[1],
-                    :imdbCode => ximdb.title[3],
-                    :formato => 'N/A',
-                    :media => 'N/A',
-                    :comment => '',
-                    :fecha => Time.now }
+  
+  if not(ximdb.es_serie)
+	k.titulo = ximdb.title2[0]
+	k.titulo_original = ximdb.title2[1]
+	k.anio = ximdb.anio[0]
+	k.imdb_code = ximdb.title2[2]
+	ximdb.writer.each  do |cada|
+		k.writers.new({:nombre => cada})
+	end
+	elcast = ximdb.cast
+  else
+	k.titulo = ximdb.title_serie[0]
+	k.titulo_original = ximdb.title_serie[1]
+	k.anio = ximdb.anio_serie[0]
+	k.imdb_code = ximdb.title_serie[2]
+	ximdb.creator.each  do |cada|
+		k.writers.new({:nombre => cada})
+	end
+	elcast = ximdb.cast_serie
+  end
+  
+#  k.titulo = ximdb.title[0]
+#  k.titulo_original = ximdb.title[2]
+#  k.anio = ximdb.title[1]
+#  k.imdb_code = ximdb.title[3]
+  k.formato = 'N/A'
+  k.media = 'N/A'
+  k.comment = ''
+  k.fecha = Time.now 
 
   ximdb.director.each  do |cada|
-    k.directors.first_or_new({:nombre => cada})
+    k.directors.new({:nombre => cada})
   end
-
-  ximdb.writer.each  do |cada|
-    k.writers.first_or_new({:nombre => cada})
-  end
+            
+#  ximdb.writer.each  do |cada|
+#    k.writers.new({:nombre => cada})
+#  end
 
   ximdb.genre.each  do |cada|
-    k.generos.first_or_new({:nombre => cada})
+    k.generos.new({:nombre => cada})
   end
 
   ximdb.country.each  do |cada|
-    k.nacions.first_or_new({:nombre => cada})
+    k.nacions.new({:nombre => cada})
   end
 
   ximdb.language.each  do |cada|
-    k.idiomas.first_or_new({:nombre => cada})
+    k.idiomas.new({:nombre => cada})
   end
 
   ximdb.sound.each  do |cada|
-    k.sonidos.first_or_new({:nombre => cada})
+    k.sonidos.new({:nombre => cada})
   end
 
   ximdb.color.each  do |cada|
-    k.colors.first_or_new({:nombre => cada})
+    k.colors.new({:nombre => cada})
   end
 
   n = 1
-  ximdb.cast.each  do |cada|
+  elcast.each  do |cada|
     k.elencos.new ({:nombre => cada[0],
                     :personaje => ((cada[1] == "" || cada[1].nil?)  ? " " : cada[1]),
                     :orden => n})
     n = n + 1
   end
 
-  ximg = "./public/posters/" + ximdb.title[3] + ".jpg"
+  ximg = "./public/posters/" + k.imdb_code + ".jpg"
   if ximdb.poster.count == 0
     FileUtils.copy_file( "./public/images/noposter.jpg",ximg)
   else
@@ -60,54 +81,54 @@ end
 
 def peliDummy
   k = Pelicula.new
-  k.attributes =  { :titulo => "",
-                    :tituloOriginal => "",
-                    :anio => "",
-                    :imdbCode => "",
-                    :formato => "N/A",
-                    :media => 'N/A',
-                    :comment => '',
-                    :fecha => Time.now }
+  k.titulo = ""
+  k.titulo_original = ""
+  k.anio = ""
+  k.imdb_code = ""
+  k.formato = "N/A"
+  k.media = 'N/A'
+  k.comment = ''
+  k.fecha = Time.now 
   k
 end
 
 def post2Peli
   k = Pelicula.new
-  k.attributes =  { :titulo => params[:Xtitulo],
-                    :tituloOriginal => params[:Xoriginal],
-                    :anio => params[:Xanio],
-                    :imdbCode => params[:Ximdb],
-                    :formato => params[:Xformato],
-                    :media => params[:Xmedia],
-                    :comment => params[:Xobserv],
-                    :fecha => Time.now }
+  k.titulo = params[:Xtitulo]
+  k.titulo_original = params[:Xoriginal]
+  k.anio = params[:Xanio]
+  k.imdb_code = params[:Ximdb]
+  k.formato = params[:Xformato]
+  k.media = params[:Xmedia]
+  k.comment = params[:Xobserv]
+  k.fecha = Time.now 
 
   separa(params[:Xdirector]).each  do |cada|
-    k.directors.first_or_new({:nombre => cada})
+    k.directors.new({:nombre => cada})
   end
 
   separa(params[:Xwriter]).each  do |cada|
-    k.writers.first_or_new({:nombre => cada})
+    k.writers.new({:nombre => cada})
   end
 
   separa(params[:Xgenero]).each  do |cada|
-    k.generos.first_or_new({:nombre => cada})
+    k.generos.new({:nombre => cada})
   end
 
   separa(params[:Xnacion]).each  do |cada|
-    k.nacions.first_or_new({:nombre => cada})
+    k.nacions.new({:nombre => cada})
   end
 
   separa(params[:Xidioma]).each  do |cada|
-    k.idiomas.first_or_new({:nombre => cada})
+    k.idiomas.new({:nombre => cada})
   end
 
   separa(params[:Xsonido]).each  do |cada|
-    k.sonidos.first_or_new({:nombre => cada})
+    k.sonidos.new({:nombre => cada})
   end
 
   separa(params[:Xcolor]).each  do |cada|
-    k.colors.first_or_new({:nombre => cada})
+    k.colors.new({:nombre => cada})
   end
 
   n = 1
